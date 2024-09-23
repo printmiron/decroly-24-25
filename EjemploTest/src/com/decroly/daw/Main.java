152bd848f6b9b242331038af6e7aa7c78f7db62d
COMPU-S040\daw1 <pavelmiron6824@gmail.com>
2024-09-23 11:29:02 +0200

    Primer ejemplo de clase

---
+++ EjemploTest/src/com/decroly/daw/Main.java
@@ -0,0 +1,34 @@
+package com.decroly.daw;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		System.out.print("Hola Mundo!");
+//		System.out.println("Hola Decroly DAW");
+		
+		int num = 0; //vale 0
+		int a = 20;
+		
+		int suma = a + num; //Tipo de dato + nombre = el valor
+		
+		double numR = 33.33;
+		
+		String text1 = "¿Como te llamas? \n";
+		String text2 = "Paco";
+		
+		System.out.print(text1+text2);
+		
+		System.out.println("test linea");
+		
+		boolean isEnable = false;
+		
+		System.out.println((a != num));
+		
+//		char c = 'S';
+		String respuesta = "Pavel";
+		System.out.println(text1 + " " + respuesta);
+		System.out.println("Edad: " + a);
+	}
+
+}
