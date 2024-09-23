8d40e904fa1cf0e63d70d34b9bad329f8b582824
PASHA\cloud <pavelmiron6824@gmail.com>
2024-09-23 20:30:54 +0200

    Ejercicio5 en casa

--- Actividad_01/src/com/decroly/daw/Main.java
+++ Actividad_01/src/com/decroly/daw/Main.java
@@ -8,41 +8,51 @@ public class Main {
 		// TODO Auto-generated method stub
 		
 		//Ejercicio1
-		System.out.println("buenos días");
-		
-		//Ejercicio2 
-		int a = 5;
-		int b = 5;
-		int c = a*b; //Resultado de la multiplicacion
-		System.out.println("5*5=" + c);
-		
-		//Ejercicio3
-		Scanner scanner = new Scanner(System.in);
-		System.out.print("Área: ");
-		
-		int lat = scanner.nextInt();
-		int area = lat*lat;
-		
-		System.out.println("Resultado es: " + area);
-		
-		//Ejercicio4
-		Scanner scanner1 = new Scanner(System.in);
-	
-		System.out.println("Primer numero: ");
-		int prim = scanner.nextInt();
-		
-		System.out.println("Segundo numero: ");
-		int segu = scanner.nextInt();
-		
-		System.out.println("Suma: " + (prim+segu));
-		System.out.println("Resta: " + (prim-segu));
-		System.out.println("Multiplicacion: " + (prim*segu));
-		System.out.println("Division: " + (prim/segu));
-		
-		
-		//Ejercicio5
-		Scanner scanner2 = new Scanner(System.in);
-		
+//		System.out.println("buenos días");
+//		
+//		//Ejercicio2 
+//		int a = 5;
+//		int b = 5;
+//		int c = a*b; //Resultado de la multiplicacion
+//		System.out.println("5*5=" + c);
+//		
+//		//Ejercicio3
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.print("Área: ");
+//		
+//		int lat = scanner.nextInt();
+//		int area = lat*lat;
+//		
+//		System.out.println("Resultado es: " + area);
+//		
+//		//Ejercicio4
+//		Scanner scanner1 = new Scanner(System.in);
+//	
+//		System.out.println("Primer numero: ");
+//		int prim = scanner.nextInt();
+//		
+//		System.out.println("Segundo numero: ");
+//		int segu = scanner.nextInt();
+//		
+//		System.out.println("Suma: " + (prim+segu));
+//		System.out.println("Resta: " + (prim-segu));
+//		System.out.println("Multiplicacion: " + (prim*segu));
+//		System.out.println("Division: " + (prim/segu));
+//		
+//		
+//		//Ejercicio5
+//		Scanner scanner2 = new Scanner(System.in);
+//		double radio;
+//	    System.out.println("Por favor, teclea el valor del radio");
+//	    radio = scanner2.nextDouble();
+//	    double circunferencia = 2 * Math.PI * radio;
+//	    double areaCirculo = Math.PI * Math.pow(radio, 2);
+//	    double volumenEsfera = (4/3) * (Math.PI * Math.pow(radio, 3));
+//	    System.out.println("La circunferencia: " + radio + " mide " + circunferencia);
+//	    System.out.println("El area circulo: " + radio + " es " + areaCirculo);
+//	    System.out.println("El volumen esfera: " + radio + " es " + volumenEsfera);
+	    
+	    //Ejercicio6
 	}
 
 }
