415817a69915acf42bc3fdba2e1b503b4ac8c358
COMPU-S040\daw1 <pavelmiron6824@gmail.com>
2024-09-25 11:36:28 +0200

    Actividad1 Hecha

--- Actividad_01/src/com/decroly/daw/Main.java
+++ Actividad_01/src/com/decroly/daw/Main.java
@@ -6,43 +6,183 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+//		//Ejercicio1
+//		System.out.println("buenos días");
+//		
+//		//Ejercicio2 
+//		int a = 5;
+//		int b = 5;
+//		int c = a*b; //Resultado de la multiplicacion
+//		System.out.println("5*5=" + c);
+//		
+//		//Ejercicio3
+//		System.out.print("Área: ");
+//		
+//		int lat = sc.nextInt();
+//		int area = lat*lat;
+//		
+//		System.out.println("Resultado es: " + area);
+//		
+//		//Ejercicio4
+//		System.out.println("Primer numero: ");
+//		int prim = sc.nextInt();
+//		
+//		System.out.println("Segundo numero: ");
+//		int segu = sc.nextInt();
+//		
+//		System.out.println("Suma: " + (prim+segu));
+//		System.out.println("Resta: " + (prim-segu));
+//		System.out.println("Multiplicacion: " + (prim*segu));
+//		System.out.println("Division: " + (prim/segu));
+//		
+//		
+//		//Ejercicio5
+//		
+//		System.out.println("Radio: ");
+//		
+//		double radio = sc.nextDouble();
+//		
+//		double circunferencia = 2 * Math.PI * radio;
+//		double area = Math.PI * Math.pow(radio, 2);
+//		double volumen = (4/3) * (Math.PI * Math.pow(radio, 3));
+//		
+//		System.out.println("La cicunferencia: " + radio + " es " + circunferencia);
+//		System.out.println("La area: " + radio + " es " + area);
+//		System.out.println("La volumen: " + radio + " es " + volumen);
+//		
+//		//Ejercicio6
+//		double articulo;
+//		double venta;
+//		
+//		System.out.println("Precio del articulo :");
+//		articulo = sc.nextDouble();
+//		
+//		System.out.println("Precio de la venta:");
+//		venta = sc.nextDouble();
+//		
+//		double descuento = (venta * 100)/articulo;
+//		System.out.println("Precio articulo: " + articulo);
+//		System.out.println("Precio venta: " + venta);
+//		System.out.println("Precio descuento: " + descuento);
+//	
+//		//Ejercicio7
+//		double metros;
+//		
+//		System.out.println("Millas: ");
+//		metros = sc.nextDouble();
+//		
+//		double millas = 1.852 * metros;
+//	
+//		System.out.println("Millas en metros: " + millas);
+//		
+//		
+//		//Ejercicio8 
+//		int num1 = 0;
+//		System.out.println("Escribe numero 1: ");
+//		num1 = sc.nextInt();
+//		
+//		int num2 = 0;
+//		System.out.println("Escribe numero 2: ");
+//		num2 = sc.nextInt();
+//		
+//		
+//		 if(num1 > num2)
+//	        {
+//	            System.out.println(num2 + ":" + num1);
+//	        }else if(num2 > num1){
+//	                System.out.println(num1 + ":" + num2);
+//	            }else{
+//	                System.out.println("Son iguales");
+//	            }
+//		
+//	    //Ejercicio9
+//		int num1 = 0;
+//		System.out.println("Escribe numero 1: ");
+//		num1 = sc.nextInt();
+//		
+//		int num2 = 0;
+//		System.out.println("Escribe numero 2: ");
+//		num2 = sc.nextInt();
+//		
+//		 if(num1 > num2)
+//		        {
+//		            System.out.println(num1);
+//		        }else if(num2 > num1){
+//		                System.out.println(num2);
+//		            }else{
+//		                System.out.println("Son iguales");
+//		            }
+//			 
+//		//Ejercicio10
+//			double num1 = 0;
+//			System.out.println("Escribe numero 1: ");
+//			num1 = sc.nextDouble();
+//			
+//			double num2 = 0;
+//			System.out.println("Escribe numero 2: ");
+//			num2 = sc.nextDouble();
+//			
+//			double num3 = 0;
+//			System.out.println("Escribe numero 3: ");
+//			num3 = sc.nextDouble();
+//			
+//			
+//			System.out.println(Math.max(num3,Math.max(num1, num2)));
+//			
+//		
+//		//Ejercicio11
+//		System.out.println("Primer numero: ");
+//		int prim = sc.nextInt();
+//		
+//		System.out.println("Segundo numero: ");
+//		int segu = sc.nextInt();
+//		
+//		System.out.println("Suma: " + (prim+segu));
+//		System.out.println("Resta: " + (prim-segu));
+//		System.out.println("Multiplicacion: " + (prim*segu));
+//		
+//		if (prim == 0){
+//		System.out.println("Division inposible");
+//		}else if (segu == 0){
+//			System.out.println("Division inposible");
+//		}else {
+//			System.out.println("Division: " + (prim/segu));
+//		}
+//		
+//		//Ejercicio12
+//		int num1 = 0;
+//		System.out.println("Escribe numero 1: ");
+//		num1 = sc.nextInt();
+//		
+//		int num2 = 0;
+//		System.out.println("Escribe numero 2: ");
+//		num2 = sc.nextInt();
+//		
+//		 if(num1 > num2)
+//		        {
+//		            System.out.println(num1);
+//		        }else if(num2 > num1){
+//		                System.out.println(num2);
+//		            }else{
+//		                System.out.println("Son iguales");
+//		            }
+//		
+		//Ejercicio13
+		int num;
+		System.out.println("Numero: ");
+		num = sc.nextInt();
 		
-		//Ejercicio1
-		System.out.println("buenos días");
+		if (num >= 0)
+		{
+			System.out.println("Numero es psitivo");
+		} else {
+			System.out.println("Numero es negativo");
+		}
 		
-		//Ejercicio2 
-		int a = 5;
-		int b = 5;
-		int c = a*b; //Resultado de la multiplicacion
-		System.out.println("5*5=" + c);
 		
-		//Ejercicio3
-		Scanner scanner = new Scanner(System.in);
-		System.out.print("Área: ");
-		
-		int lat = scanner.nextInt();
-		int area = lat*lat;
-		
-		System.out.println("Resultado es: " + area);
-		
-		//Ejercicio4
-		Scanner scanner1 = new Scanner(System.in);
+	} //Sting
+} //Main
 	
-		System.out.println("Primer numero: ");
-		int prim = scanner.nextInt();
-		
-		System.out.println("Segundo numero: ");
-		int segu = scanner.nextInt();
-		
-		System.out.println("Suma: " + (prim+segu));
-		System.out.println("Resta: " + (prim-segu));
-		System.out.println("Multiplicacion: " + (prim*segu));
-		System.out.println("Division: " + (prim/segu));
-		
-		
-		//Ejercicio5
-		Scanner scanner2 = new Scanner(System.in);
-		//...
-	}
 
-}
+
