6dee77d9d7a96fccf61a1b1e6b833ed2c6d42bd8
COMPU-S040\daw1 <pavelmiron6824@gmail.com>
2024-10-09 10:12:56 +0200

    AC4 Ej3

--- DAW/Actividad_03/src/App.java
+++ DAW/Actividad_03/src/App.java
@@ -8,106 +8,106 @@ public class App {
         Scanner sc = new Scanner(System.in);
 
         //Ejercicio1
-        // System.out.println("Escribe el numero de euros: ");
-        // int numero = sc.nextInt();
-        // if (numero % 5 != 0){
-        //     System.out.println("La cantidad tiene que ser mas que 5!");
-        // }else{
-        //     if (numero >= 500) {
-        //         System.out.println((numero / 500) + " billete/s de 500 euros");
-        //         numero %= 500;
-        //     }
-        //     if (numero >= 200) {
-        //         System.out.println((numero / 200) + " billete/s de 200 euros");
-        //         numero %= 200;
-        //     }
-        //     if (numero >= 100) {
-        //         System.out.println((numero / 100) + " billete/s de 100 euros");
-        //         numero %= 100;
-        //     }
-        //     if (numero >= 50) {
-        //         System.out.println((numero / 50) + " billete/s de 50 euros");
-        //         numero %= 50;
-        //     }
-        //     if (numero >= 20) {
-        //         System.out.println((numero / 20) + " billete/s de 20 euros");
-        //         numero %= 20;
-        //     }
-        //     if (numero >= 10) {
-        //         System.out.println((numero / 10) + " billete/s de 10 euros");
-        //         numero %= 10;
-        //     }
-        //     if (numero >= 5) {
-        //         System.out.println((numero / 5) + " billete/s de 5 euros");
-        //         numero %= 5;
-        //     }
-        // }
-        //Ejercicio2
-        int opcion;
-
-        do {
-
-            System.out.println("Menu opciones: ");
-            System.out.println("1. Suma");
-            System.out.println("2. Restar");
-            System.out.println("3. Multiplicar");
-            System.out.println("4. Dividir");
-            System.out.println("5. Salir");
-            System.out.println("Seleciona una opcion!");
-            opcion = sc.nextInt();
-
-            switch (opcion) {
-                case 1:
-
-                    System.out.println("Introduce primer numero: ");
-                    double num1 = sc.nextDouble();
-                    System.out.println("Introduce segundo numero: ");
-                    double num2 = sc.nextDouble();
-                    System.out.println("Resultado es: " + (num1 + num2));
-                    break;
-
-                case 2:
-
-                    System.out.println("Introduce primer numero: ");
-                    num1 = sc.nextDouble();
-                    System.out.println("Introduce segundo numero: ");
-                    num2 = sc.nextDouble();
-                    System.out.println("Resultado es: " + (num1 - num2));
-                    break;
-
-                case 3:
-
-                    System.out.println("Introduce primer numero: ");
-                    num1 = sc.nextDouble();
-                    System.out.println("Introduce segundo numero: ");
-                    num2 = sc.nextDouble();
-                    System.out.println("Resultado es: " + (num1 * num2));
-                    break;
-
-                case 4:
-
-                    System.out.println("Introduce primer numero: ");
-                    num1 = sc.nextDouble();
-                    System.out.println("Introduce segundo numero: ");
-                    num2 = sc.nextDouble();
-
-                    if (num2 == 0) {
-                        System.out.println("No es posible");
-                    } else {
-                        System.out.println("Resultado es: " + (num1 / num2));
-                    }
-
-                case 5:
-
-                    System.out.println("Salir de programa");
-                    break;
-
-                default:
-                    System.out.println("Opcion no valida!");
-                    throw new AssertionError();
+        System.out.println("Escribe el numero de euros: ");
+        int numero = sc.nextInt();
+        if (numero % 5 != 0){
+            System.out.println("La cantidad tiene que ser mas que 5!");
+        }else{
+            if (numero >= 500) {
+                System.out.println((numero / 500) + " billete/s de 500 euros");
+                numero %= 500;
             }
+            if (numero >= 200) {
+                System.out.println((numero / 200) + " billete/s de 200 euros");
+                numero %= 200;
+            }
+            if (numero >= 100) {
+                System.out.println((numero / 100) + " billete/s de 100 euros");
+                numero %= 100;
+            }
+            if (numero >= 50) {
+                System.out.println((numero / 50) + " billete/s de 50 euros");
+                numero %= 50;
+            }
+            if (numero >= 20) {
+                System.out.println((numero / 20) + " billete/s de 20 euros");
+                numero %= 20;
+            }
+            if (numero >= 10) {
+                System.out.println((numero / 10) + " billete/s de 10 euros");
+                numero %= 10;
+            }
+            if (numero >= 5) {
+                System.out.println((numero / 5) + " billete/s de 5 euros");
+                numero %= 5;
+            }
+        }
+        //Ejercicio2
+        // int opcion;
 
-        } while (opcion != 5);
+        // do {
+
+        //     System.out.println("Menu opciones: ");
+        //     System.out.println("1. Suma");
+        //     System.out.println("2. Restar");
+        //     System.out.println("3. Multiplicar");
+        //     System.out.println("4. Dividir");
+        //     System.out.println("5. Salir");
+        //     System.out.println("Seleciona una opcion!");
+        //     opcion = sc.nextInt();
+
+        //     switch (opcion) {
+        //         case 1:
+
+        //             System.out.println("Introduce primer numero: ");
+        //             double num1 = sc.nextDouble();
+        //             System.out.println("Introduce segundo numero: ");
+        //             double num2 = sc.nextDouble();
+        //             System.out.println("Resultado es: " + (num1 + num2));
+        //             break;
+
+        //         case 2:
+
+        //             System.out.println("Introduce primer numero: ");
+        //             num1 = sc.nextDouble();
+        //             System.out.println("Introduce segundo numero: ");
+        //             num2 = sc.nextDouble();
+        //             System.out.println("Resultado es: " + (num1 - num2));
+        //             break;
+
+        //         case 3:
+
+        //             System.out.println("Introduce primer numero: ");
+        //             num1 = sc.nextDouble();
+        //             System.out.println("Introduce segundo numero: ");
+        //             num2 = sc.nextDouble();
+        //             System.out.println("Resultado es: " + (num1 * num2));
+        //             break;
+
+        //         case 4:
+
+        //             System.out.println("Introduce primer numero: ");
+        //             num1 = sc.nextDouble();
+        //             System.out.println("Introduce segundo numero: ");
+        //             num2 = sc.nextDouble();
+
+        //             if (num2 == 0) {
+        //                 System.out.println("No es posible");
+        //             } else {
+        //                 System.out.println("Resultado es: " + (num1 / num2));
+        //             }
+
+        //         case 5:
+
+        //             System.out.println("Salir de programa");
+        //             break;
+
+        //         default:
+        //             System.out.println("Opcion no valida!");
+        //             throw new AssertionError();
+        //     }
+
+        // } while (opcion != 5);
 
     }
 }
--- DAW/Actividad_04/src/App.java
+++ DAW/Actividad_04/src/App.java
@@ -7,6 +7,42 @@ public class App {
         Scanner sc = new Scanner(System.in);
 
         //Ejercicio1
+        // double numero [] = new double [10];
+        
+        // System.out.println("Insertar 10 numeros: ");
+        
+        // for (int i = 0; i < 10; i++) {
+        //     System.out.println("Numero " + ( i+1 ) + ":");
+        //     numero[i] = sc.nextDouble();
+        // }
+
+        // sc.close();
+
+        // for (int i = 0; i < 10; i++) {
+        //     System.out.println(numero[i]);
+        // }
+
+
+        //Ejercicio2
+        // int suma = 0;
+        // double numero [] = new double [10];
+        
+        // System.out.println("Insertar 10 numeros: ");
+        
+        // for (int i = 0; i < 10; i++) {
+        //     System.out.println("Numero " + ( i+1 ) + ":");
+        //     numero[i] = sc.nextDouble();
+        // }
+
+        // for (int i = 0; i < 10; i++) {
+        //         suma += numero[i];
+        //     }
+    
+        //     System.out.println("Suma total es: " + suma);
+
+        //Ejercicio3
+        double max = numero[0];
+        double min = numero[0];
         double numero [] = new double [10];
         
         System.out.println("Insertar 10 numeros: ");
@@ -16,10 +52,18 @@ public class App {
             numero[i] = sc.nextDouble();
         }
 
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Numero: " + ( i+1 ) + ":" + numero[i]);
+        
+        for (int i = 0; i < numero.length; i++) {
+            if (numero[i] > max) {
+                max = numero[i];
+            }
+
+            if (numero[i] > min) {
+                min = numero[i];
+            }
         }
 
-        sc.close();
+        System.out.println("Numero max: " + max);
+        System.out.println("Numero min: " + min);
     }
 }
