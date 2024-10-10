8b029e4d33b73e1b664d9715974c40f17f2eed24
COMPU-S040\daw1 <pavelmiron6824@gmail.com>
2024-10-10 11:00:14 +0200

    AC4

--- DAW/Actividad_04/src/App.java
+++ DAW/Actividad_04/src/App.java
@@ -41,29 +41,96 @@ public class App {
         //     System.out.println("Suma total es: " + suma);
 
         //Ejercicio3
-        double max = numero[0];
-        double min = numero[0];
-        double numero [] = new double [10];
+        // double numero [] = new double [10];
         
-        System.out.println("Insertar 10 numeros: ");
+        // System.out.println("Insertar 10 numeros: ");
         
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Numero " + ( i+1 ) + ":");
-            numero[i] = sc.nextDouble();
-        }
+        // for (int i = 0; i < 10; i++) {
+        //     System.out.println("Numero " + ( i+1 ) + ":");
+        //     numero[i] = sc.nextDouble();
+        // }
+        // double max = numero[0];
+        // double min = numero[0];
+        
+        // for (int i = 0; i < numero.length; i++) {
+        //     if (numero[i] > max) {
+        //         max = numero[i];
+        //     }
 
+        //     if (numero[i] < min) {
+        //         min = numero[i];
+        //     }
+        // }
+
+        // System.out.println("Numero max: " + max);
+        // System.out.println("Numero min: " + min);
+
+        //Ejercicio4
+
+        // int positivos = 0;
+        // int negativos = 0;
+        // int numero [] = new int [20];
         
+        // System.out.println("Insertar 10 numeros: ");
+        
+        // for (int i = 0; i < 20; i++) {
+        //     System.out.println("Numero " + ( i+1 ) + ":");
+        //     numero[i] = sc.nextInt();
+        // }
+        // for (int i = 0; i < numero.length; i++) {  
+        //     if (numero[i] > 0) {
+        //         positivos += numero[i];
+        //     }
+
+        //     if (numero[i] < 0) {
+        //         negativos += numero[i];
+        //     }
+        // }
+
+        //     System.out.println("Suma de numeros positivos: " + positivos);
+        //     System.out.println("Suma de numeros negativos: " + negativos);
+
+        //Ejercicio5
+        // int suma = 0;
+        // int media = 0;
+        // int numero [] = new int [20];
+        // System.out.println("Insertar 10 numeros: ");
+        
+        // for (int i = 0; i < 20; i++) {
+        //     System.out.println("Numero " + ( i+1 ) + ":");
+        //     numero[i] = sc.nextInt();
+        // }
+
+        // for (int i = 0; i < numero.length; i++) {
+        //     suma += numero[i];
+        // }
+        // media = suma / numero.length;
+
+        // System.out.println("La media es: " + media);
+
+        //Ejercicio6
+
+        int numN = 0;
+        int numM = 0;
+        int length = 0;
+        
+        int numero [] = new int [length];
+        System.out.println("Insertar numero N de posiciones: ");
+
         for (int i = 0; i < numero.length; i++) {
-            if (numero[i] > max) {
-                max = numero[i];
-            }
-
-            if (numero[i] > min) {
-                min = numero[i];
-            }
+            
         }
 
-        System.out.println("Numero max: " + max);
-        System.out.println("Numero min: " + min);
+        
+
+
+
+
+
+
+
+
+
+
     }
 }
--- DAW/Actividad_04/src/tempCodeRunnerFile.java
+++ DAW/Actividad_04/src/tempCodeRunnerFile.java
@@ -1,1 +1,4 @@
-numero[i]
+for (int i = 0; i < 10; i++) {
+            System.out.println("Numero " + ( i+1 ) + ":");
+            numero[i] = sc.nextDouble();
+        }
