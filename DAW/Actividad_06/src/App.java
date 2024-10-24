043c29476d091929fc820f4d3a487686a115aeae
COMPU-S040\daw1 <pavelmiron6824@gmail.com>
2024-10-24 08:57:13 +0200

    AC 6 Ej 3

---
+++ DAW/Actividad_06/src/App.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Scanner sc = new Scanner(System.in);
+
+        //Ejercicio1
+
+        // int matriz [][] = new int [5][5];
+
+        // int num = 1;
+
+        // for (int i = 0; i < 5; i++) {
+        //     for (int j = 0; j < 5; j++) {
+        //         matriz[i][j] = num ;
+        //         num++;
+        //     }
+        // }
+
+        // for (int i = 0; i < 5; i++) {
+        //     for (int j = 0; j < 5; j++) {
+        //         System.out.println(matriz[i][j]);
+        //     }
+        //     System.out.println();
+        // }
+
+        //Ejercicio2
+
+        // int matriz [][] = new int [10][10];
+
+        // int num = 1;
+
+        // for (int i = 0; i < matriz.length; i++) {
+        //    for (int j = 0; j < matriz.length; j++) {
+        //        matriz[i][j] = num * ( j + 1 );
+        //        num++;
+        //    } 
+        // }
+
+        // for (int i = 0; i < matriz.length; i++) {
+        //     for (int j = 0; j < matriz.length; j++) {
+        //         System.out.println(matriz[i][j]);
+        //     }
+        //     System.out.println();
+        // }
+
+        //Ejercicio3
+
+        
+        
+
+        System.out.println("Introduce el tamaño N: ");
+        int N = sc.nextInt();
+
+        System.out.println("Introduce el tamaño M: ");
+        int M = sc.nextInt();
+
+        int materiz [][] = new int [N][M];
+
+        
+
+
+
+
+
+
+
+
+
+
+    }
+}
